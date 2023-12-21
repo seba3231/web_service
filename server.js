1459c61c9e1d0132164e78290087cae9cf684e97
Sebastian Pelaez <pelaez.seba@gmail.com>
2023-12-21 10:53:50 -0300

    usuario de gestion ambiental

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+// Array que almacenar usuarios y contraseñas
+const usuarios = [
+    { usuario: 'admin', contraseña: 'wmia123' },
+    { usuario: 'gestionambiental', contraseña: 'wmia678' }
+  ];
+  
 
 app.get('/', (req, res) => {
     var user = req.query.user
     var password = req.query.password
-    if (user == "admin" && password == "admin123") {
+    var usuarioEncontrado = autenticarUsuario(user,password)
+    if (usuarioEncontrado) {
         res.send(true)
     } else {
         res.send(false)
@@ -16,3 +23,14 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
+
+// Función para autenticar usuarios
+function autenticarUsuario(usuario, contraseña) {
+    const usuarioEncontrado = usuarios.find(u => u.usuario === usuario && u.contraseña === contraseña);
+  
+    if (usuarioEncontrado) {
+      return true; // Autenticación exitosa
+    } else {
+      return false; // Autenticación fallida
+    }
+  }
